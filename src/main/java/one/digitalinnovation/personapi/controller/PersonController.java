a029c6589368eae0722bc92969f6a22922e8d69b
veraueda <veraueda@hotmail.com>
2021-09-16 00:12:56 -0300

    Updated new files after review

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -12,7 +12,7 @@ public interface PersonMapper {
 
         @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
 
-        Person toModel(PersonDTO dto);
+        Person toModel(PersonDTO personDTO);
 
-        PersonDTO toDTO(Person dto);
+        PersonDTO toDTO(Person person);
 }
